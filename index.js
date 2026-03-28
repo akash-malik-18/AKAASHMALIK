1c75664aafb8b8a741131a78711b7645b0ee0df8
Akash_Malik <98683768+akash-malik-18@users.noreply.github.com>
2026-03-28 18:37:56 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const io = require("socket.io")(process.env.PORT || 3000, {
   cors: { origin: "*" }
 });
+
+console.log("Server is Live!");
+
 io.on("connection", (socket) => {
+  console.log("Device connected: " + socket.id);
   socket.on("audio_data", (data) => {
     socket.broadcast.emit("receive_audio", data);
   });
